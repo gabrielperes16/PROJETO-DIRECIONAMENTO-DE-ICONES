3c86af7ecb4f459d2d66eee370e0013318681b96
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-04-25 17:12:42 -0300

    Add files via upload

--- PROJETO DIRECIONAMENTO DE LINKS/main.js
+++ PROJETO DIRECIONAMENTO DE LINKS/main.js
@@ -1,19 +1,18 @@
-
-
+// Esta função Verifica se o elemento observado esta ou não visível na página//
 const Observador = new IntersectionObserver((FORNECEDOR_DE_INFORMAÇÕES) => {
-    FORNECEDOR_DE_INFORMAÇÕES.forEach((verificador) =>{    
-        
-        if(verificador.isIntersecting){
+    FORNECEDOR_DE_INFORMAÇÕES.forEach((verificador) => {
+        // Quando um elemento entra na é visível, a classe CSS adiconar_função é adicionada//
+        if (verificador.isIntersecting) {
             verificador.target.classList.add('adiconar_função')
         }
-        else{
+        else {
             verificador.target.classList.remove('adiconar_função')
         }
+    })
 })
- })
-
-const elements= document.querySelectorAll(".hidden")
-
+//Todos os elementos da classe .hidden são selecionados.
+const elements = document.querySelectorAll(".hidden")
+//O observador de interseção começa a observar cada um desses elementos.
 elements.forEach((Element) => Observador.observe(Element))
 
 
